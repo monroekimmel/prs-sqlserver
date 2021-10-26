10dd8cb4c704bcb39b927ba9ff64699c3c10014e
monroekimmel <monroekimmel@gmail.com>
2021-10-25 20:53:23 -0400

    Added more tables

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,30 @@
+DROP DATABASE IF EXISTS prs;
 CREATE DATABASE prs;
+
+CREATE TABLE Users
+(	ID					INT				PRIMARY KEY IDENTITY,
+	FirstName			VARCHAR(50)		NOT NULL,
+	LastName			VARCHAR(50)		NOT NULL,
+	Phone				VARCHAR(13)		NOT NULL,
+	Email				VARCHAR (75)	NOT NULL,
+	Username			VARCHAR(20)		NOT NULL,
+	Password			VARCHAR(20)		NOT NULL,
+	Reviewer			BIT				NOT NULL,
+	Admin				BIT				NOT NULL
+	);
+
+CREATE TABLE PurchaseRequests
+(	ID					INT					PRIMARY KEY IDENTITY,
+	UserID				INT				NOT NULL,
+	Description			VARCHAR(100)	NOT NULL,
+	Justification		VARCHAR(255)	NOT NULL,
+	DateNeeded			VARCHAR(75)		NOT NULL,
+	DeliveryMode		VARCHAR(25)		NOT NULL,
+	Status				VARCHAR(20)		NOT NULL,
+	Total				SMALLMONEY		NOT NULL,
+	SubmittedDate		DATETIME2		NOT NULL,
+	ReasonForRejection	VARCHAR(255),
+	CONSTRAINT FK_PRUser FOREIGN KEY (UserID)
+	REFERENCES Users(ID)
+	);
+
